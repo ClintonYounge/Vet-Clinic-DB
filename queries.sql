2b2c2f6f46f7786d1190df11d78a6d1c2d2b3f56
ClintonYounge <clintonyounge18@gmail.com>
2023-07-13 02:13:29 -0400

    Update queries code

--- queries.sql
+++ queries.sql
@@ -1,4 +1,3 @@
--- Day 1 queries
 SELECT * FROM animals WHERE name LIKE '%mon';
 SELECT name FROM animals WHERE EXTRACT(year FROM date_of_birth) BETWEEN 2016 AND 2019;
 SELECT * FROM animals WHERE neutered = true AND escape_attempts < 3;
@@ -8,7 +7,42 @@ SELECT * FROM animals WHERE neutered = true;
 SELECT * FROM animals WHERE name != 'Gabumon';
 SELECT * FROM animals WHERE weight_kg BETWEEN 10.4 AND 17.3;
 
--- Day 2 queries
+
+-- Added Updates and Queries
+BEGIN WORK;
+    UPDATE ANIMALS
+    SET species = 'unspecified'
+    SELECT * FROM animals;
+ROLLBACK;
+
+BEGIN WORK;
+    UPDATE animals
+    SET species = 'digimon'
+    WHERE name LIKE '%mon';
+
+    UPDATE animals
+    SET species = 'pokemon'
+    WHERE species IS NULL;
+COMMIT WORK;
+SELECT * FROM animals;
+
+BEGIN WORK;
+    DELETE FROM animals;
+ROLLBACK;
+SELECT * FROM animals;
+
+BEGIN WORK;
+SAVEPOINT sp1;
+DELETE FROM animals WHERE date_of_birth > '2022/01/01';
+SAVEPOINT sp2;
+UPDATE animals SET weight_kg = weight_kg * -1;
+SAVEPOINT SP3;
+ROLLBACK TO SP2;
+UPDATE animals SET weight_kg = weight_kg * -1 WHERE weight_kg < 0;
+COMMIT;
+SELECT * FROM animals;
+
+
 SELECT COUNT(*) AS animals_count FROM animals;
 SELECT COUNT(*) AS escape_attempts_count FROM animals 
 WHERE escape_attempts = 0; 
